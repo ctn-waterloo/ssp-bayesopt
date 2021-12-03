Fixed bugs in the acquisition funciton and gradient function.  Did not include lenghtscales.

ssp_bayes_opt/agent.py
@@ -12,6 +12,27 @@ from . import blr
 
 import functools
 
+def factory(agent_type, init_xs, init_ys):
+
+    data_dim = init_xs.shape[1]
+
+    agt = None
+    if agent_type == 'ssp-mi':
+        ptrs, K_scale_rotates = ssp.RandomBasis(dim=data_dim, d=128)
+        agt = SSPAgent(init_xs, init_ys, ptrs, K_scale_rotates) 
+    elif agent_type == 'hex-mi':
+        ptrs, K_scale_rotates = ssp.HexagonalBasis(dim=data_dim,
+                                                   n_rotates=2,
+                                                   n_scales=3,
+                                                   scale_min=0.8,
+                                                   scale_max=3.4)
+        agt = SSPAgent(init_xs, init_ys, ptrs, K_scale_rotates) 
+    elif agent_type == 'gp-mi':
+        agt = GPAgent(init_xs, init_ys)
+    else:
+        raise RuntimeWarning(f'Undefined agent type {agent_type}')
+    return agt
+
 class Agent:
     def __init__(self):
         pass
@@ -79,19 +100,21 @@ class GPAgent:
         return min_func, None
 
 class SSPAgent:
-    def __init__(self, init_xs, init_ys, n_scales=3, n_rotates=2, scale_min=0.8, scale_max=3.4):
+    def __init__(self, init_xs, init_ys, ptrs, K_scale_rotates):
   
         (num_pts, data_dim) = init_xs.shape
 
-        # Create the simplex.
-        self.ptrs, K_scale_rotates = ssp.HexagonalBasis(dim=data_dim)
-#         self.ptrs, K_scale_rotates = ssp.RandomBasis(dim=data_dim, d=128)
-        self.ptrs = np.vstack(self.ptrs)
+        self.ptrs = np.vstack(ptrs)
+#         self.ptrs = np.vstack(self.ptrs)
+        self.K_scale_rotates = K_scale_rotates
+
+
         self.ssp_dim = self.ptrs.shape[1]
 
         # Optimize the length scales
         self.length_scale = self._optimize_lengthscale(init_xs, init_ys)
         print('Selected Lengthscale: ', self.length_scale)
+#         exit()
 
         # Encode the initial sample points 
         init_phis = self.encode(init_xs)
@@ -105,19 +128,25 @@ class SSPAgent:
         self.sqrt_alpha = np.log(2/1e-6)
 
         # Cache for the input xs.
-        self.phis = None
+#         self.phis = None
 
     ### end __init__
 
     def encode(self, xs):
         return self._encode(self.ptrs, xs, length_scale=self.length_scale)
 
+
     def _optimize_lengthscale(self, init_xs, init_ys):
 
-        ls_0 = 20. * np.ones((init_xs.shape[1],))
+#         ls_0 = 20. * np.ones((init_xs.shape[1],))
+        ls_0 = 8. * np.ones((init_xs.shape[1],))
 
         def min_func(length_scale):
             init_phis = self._encode(self.ptrs, init_xs, np.abs(length_scale))
+
+#             W = np.linalg.pinv(init_phis) @ init_ys
+#             mu = np.dot(init_phis,W)
+
             b = blr.BayesianLinearRegression(self.ssp_dim)
             b.update(init_phis, init_ys)
             mu, var = b.predict(init_phis)
@@ -133,10 +162,11 @@ class SSPAgent:
 
 
     def eval(self, xs):
-        if self.phis is None:
-            self.phis = self.encode(xs)
-        ### end if
-        mu, var = self.blr.predict(self.phis)
+#         if self.phis is None:
+#             self.phis = self.encode(xs)
+#         ### end if
+        phis = self.encode(xs)
+        mu, var = self.blr.predict(phis)
         phi = self.sqrt_alpha * (np.sqrt(var + self.gamma_t) - np.sqrt(self.gamma_t)) 
         return mu, var, phi
 
@@ -150,30 +180,32 @@ class SSPAgent:
         def optim_func(x, m=self.blr.m,
                        sigma=self.blr.S,
                        gamma=self.gamma_t,
-                       beta_inv=self.blr.beta,
+                       beta_inv=1/self.blr.beta,
                        ptrs = self.ptrs,
+                       length_scale = self.length_scale,
                        ):
-#             ptr = self.encode(x.reshape(1,-1))
-            ptr = ssp.vector_encode(ptrs, x.reshape(1,-1))
+            ptr = ssp.vector_encode(ptrs, x.reshape(1,-1) @ np.diag(1/length_scale))
             val = ptr @ m
             mi = np.sqrt(gamma + beta_inv + ptr @ sigma @ ptr.T) - np.sqrt(gamma)
             return -(val + mi).flatten()
         ### end optim_func
 
+
         def jac_func(x, m=self.blr.m,
                      sigma=self.blr.S,
                      gamma=self.gamma_t,
-                     beta_inv=self.blr.beta,
+                     beta_inv=1/self.blr.beta,
                      ptrs=self.ptrs,
+                     length_scale=self.length_scale,
                      ):
-#             ptr = self.encode(x.reshape(1,-1))
-            ptr = ssp.vector_encode(ptrs, x.reshape(1,-1))
+            ptr, grad_ptr = ssp.vector_encode(ptrs, x.reshape(1,-1) @ np.diag(1/length_scale), do_grad=True)
             sqr = (ptr @ sigma @ ptr.T) 
             scale = np.sqrt(sqr + gamma + beta_inv)
-            retval = -(m.flatten() + sigma @ ptr.T / scale)
+            retval = grad_ptr.squeeze().T @ -(m + sigma @ ptr.T / scale) 
+#             retval = -(m + sigma @ ptr.T / scale) 
             return retval
         ### end gradient
-        return optim_func, None #jac_func 
+        return optim_func, jac_func 
     ### end select_optimal
 
     def update(self, x_t:np.ndarray, y_t:np.ndarray, sigma_t:float):
@@ -191,7 +223,7 @@ class SSPAgent:
         # Update BLR
         # TODO: use vector encode
 #         ptr = ssp.vector_encode(ptrs, x.reshape(1,-1))
-        phi = self.encode(x_val)
+        phi = np.atleast_2d(self.encode(x_val).squeeze())
         self.blr.update(phi, y_val)
         
         # Update gamma

ssp_bayes_opt/bayesian_optimization.py
@@ -8,10 +8,11 @@ from scipy.optimize import minimize
 from typing import Callable
 
 class BayesianOptimization:
-    def __init__(self, f: Callable[...,float] =None, pbounds: dict =None, 
+    def __init__(self, f: Callable[...,float] =None, pbounds: np.ndarray=None, 
                  random_state: int =None, verbose: bool=False):
         assert not f is None, 'Must specify a callable target function'
         assert not pbounds is None, 'Must dictionary of input bounds'
+        assert pbounds.shape[1] == 2, 'Must specify bounds of form [lower, upper] for each data dimension'
 
         if not random_state is None:
             np.random.seed(random_state)
@@ -19,8 +20,7 @@ class BayesianOptimization:
         self.target = f
         self.bounds = pbounds
 
-        self.num_dims = len(self.bounds.keys())
-        self.num_decoding = 10000
+        self.num_dims = self.bounds.shape[0]
 
         self.xs = None
         self.ys = None
@@ -32,17 +32,13 @@ class BayesianOptimization:
 
 
         init_xs = self._sample_domain(num_points=init_points)
-        arg_names = self.bounds.keys()
+#         arg_names = self.bounds.keys()
 
-        init_ys = np.array([self.target(**dict(zip(arg_names, x))) for x in init_xs]).reshape((init_points,-1))
+#         init_ys = np.array([self.target(**dict(zip(arg_names, x))) for x in init_xs]).reshape((init_points,-1))
+        init_ys = np.array([self.target(np.atleast_2d(x)) for x in init_xs]).reshape((init_points,-1))
 
         # Initialize the agent
-        if agent_type == 'ssp-mi':
-            agt = agent.SSPAgent(init_xs, init_ys) 
-        elif agent_type == 'gp-mi':
-            agt = agent.GPAgent(init_xs, init_ys)
-        else:
-            raise RuntimeWarning(f'Undefined agent type {agent_type}')
+        agt = agent.factory(agent_type, init_xs, init_ys)
 
         self.times = np.zeros((n_iter,))
         self.xs = []
@@ -54,7 +50,9 @@ class BayesianOptimization:
 
 
         # Extract the upper and lower bounds of domain for sampling.
-        lbounds, ubounds = list(zip(*[self.bounds[k] for k in self.bounds.keys()]))
+#         lbounds, ubounds = list(zip(*[self.bounds[k] for k in self.bounds.keys()])),
+        lbounds = self.bounds[:,0]
+        ubounds = self.bounds[:,1]
 
         print('| iter\t | target\t | x\t |')
         print('-------------------------------')
@@ -73,46 +71,49 @@ class BayesianOptimization:
                
                 x_init = np.random.uniform(low=lbounds, high=ubounds, size=(len(ubounds),))
 #                 if res_idx == 0 and len(self.xs) > 0:
-                if len(self.xs) > 0:
-                    alpha = 0.9**t
-                    x_init =  alpha * x_init + (1-alpha) * self.xs[np.argmax(self.ys)].flatten()
+#                 if len(self.xs) > 0:
+#                     alpha = 0.9**t
+#                     x_init =  alpha * x_init + (1-alpha) * self.xs[np.argmax(self.ys)].flatten()
 
 
                 # Do bounded optimization to ensure x stays in bound
                 soln = minimize(optim_func, x_init,
                                 jac=jac_func, 
                                 method='L-BFGS-B', 
-                                bounds=[self.bounds[k] for k in self.bounds.keys()])
+                                bounds=self.bounds)
                 vals.append(-soln.fun)
                 solns.append(np.copy(soln.x))
             self.times[t] = time.thread_time_ns() - start
             ## END timing section
 
             best_val_idx = np.argmax(vals)
-            x_t = solns[best_val_idx].reshape((1,-1))
+#             x_t = solns[best_val_idx].reshape((1,-1))
+            x_t = np.atleast_2d(solns[best_val_idx].flatten())#.reshape((1,-1))
+
             mu_t, var_t, phi_t = agt.eval(x_t)
 
             # Log actions
-            query_point = dict(zip(arg_names, x_t.flatten()))
-            y_t = np.array([[self.target(**query_point)]])
+            query_point = np.atleast_2d(x_t.flatten())
+#             y_t = np.array([[self.target(query_point)]])
+            y_t = np.atleast_2d(self.target(query_point))
 
             print(f'| {t}\t | {y_t}\t | {query_point}\t |')
             agt.update(x_t, y_t, var_t)
 
             # Log actions
-            self.xs.append(x_t)
-            self.ys.append(y_t)
+            self.xs.append(np.copy(x_t))
+            self.ys.append(np.copy(y_t))
 
         ### end for t in range(num_iters)
 
         pass
 
     def _sample_domain(self, num_points: int=10) -> np.ndarray:
-        lbounds, ubounds = zip(*[self.bounds[x] for x in self.bounds.keys()])
+#         lbounds, ubounds = zip(*[self.bounds[x] for x in self.bounds.keys()])
 
         sampler = qmc.Sobol(d=self.num_dims) 
         u_sample_points = sampler.random(num_points)
-        sample_points = qmc.scale(u_sample_points, lbounds, ubounds)
+        sample_points = qmc.scale(u_sample_points, self.bounds[:,0], self.bounds[:,1])
         return sample_points
 
     @property 