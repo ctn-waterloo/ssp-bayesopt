805143325c8ebdb9eb8ea283bcafc409bac2dcbf
Michael Furlong <furlong@gmail.com>
2022-01-16 17:19:08 -0500

    Minor test reorganization

--- tests/test_ssp_bo.py
+++ tests/test_ssp_bo.py
@@ -61,7 +61,9 @@ cum_regrets = []
 num_trials = 4
 for trial in range(num_trials):
 #     r = SSPBayesOptTrial().run(**{'function_name':'branin-hoo', 'algorithm':'ssp-mi'})
-    r = SSPBayesOptTrial().run(**{'function_name':'himmelblau', 'algorithm':'ssp-hex'})
+    r = SSPBayesOptTrial().run(**{'function_name':'himmelblau', 
+                                  'algorithm':'ssp-hex',
+                                  'num_restarts':10})
 #     cum_reg = np.divide(np.cumsum(r['regret']), np.arange(1, len(r['regret'])+1))
     cum_reg = r['regret']
     cum_regrets.append(cum_reg)
