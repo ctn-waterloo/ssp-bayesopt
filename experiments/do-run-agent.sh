99865906f45bc6a8d23390d34f3f924414f70fdd
Michael Furlong <furlong@gmail.com>
2022-05-13 19:02:33 -0400

    Bash script that sets the arguments for the experiments

---
+++ experiments/do-run-agent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dest_dir="$HOME/Public/data/ssp-bayesopt/profiling"
+echo $dest_dir
+
+for func in "branin-hoo" "himmelblau" "goldstein-price"
+do
+	for agt in "ssp-rand" "ssp-hex"
+	do
+		python run_agent.py --agent $agt --num-trials 30 --func $func --data-dir $dest_dir
+	done
+done
