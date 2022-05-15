35ed2f89fd542888bb5e0b5e9c521a8b0ab51c6b
P. Michael Furlong <michael.furlong@uwaterloo.ca>
2022-05-14 20:32:21 -0400

    Added flag so lengthscale optimizes

--- experiments/do-run-agent.sh
+++ experiments/do-run-agent.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-dest_dir="$HOME/Public/data/ssp-bayesopt/profiling"
+dest_dir="/run/media/furlong/Data/ssp-bayesopt/timing"
 echo $dest_dir
 
 for func in "branin-hoo" "himmelblau" "goldstein-price"
 do
 	for agt in "ssp-rand" "ssp-hex"
 	do
-		python run_agent.py --agent $agt --num-trials 30 --func $func --data-dir $dest_dir
+		python run_agent.py --agent $agt --num-trials 30 --func $func --data-dir $dest_dir --len-scale -1
 	done
 done
