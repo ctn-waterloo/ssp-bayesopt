c302a60ffe55fc194b096929c31455a3f6a91850
Michael Furlong <furlong@gmail.com>
2022-05-14 22:23:28 -0400

    Modified do-run-agent.sh to use an automatically optimized lengthscale

--- experiments/do-run-agent.sh
+++ experiments/do-run-agent.sh
@@ -7,6 +7,6 @@ for func in "branin-hoo" "himmelblau" "goldstein-price"
 do
 	for agt in "ssp-rand" "ssp-hex"
 	do
-		python run_agent.py --agent $agt --num-trials 30 --func $func --data-dir $dest_dir
+		python run_agent.py --agent $agt --num-trials 30 --func $func --data-dir $dest_dir --len-scale -1
 	done
 done
