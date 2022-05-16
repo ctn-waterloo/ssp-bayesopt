d083a3b6f0b48ebad7bd710520e9592a51f1db56
P. Michael Furlong <michael.furlong@uwaterloo.ca>
2022-05-16 17:19:29 -0400

    Updated plotting scripts

--- experiments/lengthscale_fitting_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dest_dir="tmp-data/`git branch --show-current`"
-echo $dest_dir
-
-for agt in "ssp-rand" "ssp-hex"
-do
-	python run_agent.py --agent $agt --num-trials 30 --func branin-hoo --data-dir $dest_dir
-done
--- util/do-compare-algs.sh
+++ util/do-compare-algs.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-# sinc_gp_dir=/run/media/furlong/Data/ssp-bayesopt/test-funcs
-# ssp_dir=/run/media/furlong/Data/ssp-bayesopt/lenscale-fitting
-ssp_dir=/run/media/furlong/Data/ssp-bayesopt/lenscale-optim
-sinc_gp_dir=/run/media/furlong/Data/ssp-bayesopt/static-sinc/
-matern_gp_dir=/run/media/furlong/Data/ssp-bayesopt/bak.test-funcs
+ssp_dir=/run/media/furlong/Data/ssp-bayesopt/test-funcs
+sinc_gp_dir=/run/media/furlong/Data/ssp-bayesopt/test-funcs
+matern_gp_dir=/run/media/furlong/Data/ssp-bayesopt/test-funcs
 
 for func in himmelblau branin-hoo goldstein-price
-# for func in branin-hoo 
 do
 	
-	python compare-algs.py --alg "SSP-Rand" $ssp_dir/$func/ssp-rand --baseline "GP-Matern" $matern_gp_dir/$func/matern.static-gp | tee -a $func-stats.txt
+	python compare-algs.py --alg "SSP-Rand" $ssp_dir/$func/ssp-rand --baseline "GP-Matern" $matern_gp_dir/$func/gp-matern | tee -a $func-stats.txt
 
-	python compare-algs.py --alg "SSP-Hex" $ssp_dir/$func/ssp-hex --baseline "GP-Matern" $matern_gp_dir/$func/matern.static-gp | tee -a $func-stats.txt
+	python compare-algs.py --alg "SSP-Hex" $ssp_dir/$func/ssp-hex --baseline "GP-Matern" $matern_gp_dir/$func/gp-matern | tee -a $func-stats.txt
 
-	python compare-algs.py --alg "SSP-Rand" $ssp_dir/$func/ssp-rand --baseline "GP-Sinc" $sinc_gp_dir/$func/static-gp | tee -a $func-stats.txt
+	python compare-algs.py --alg "SSP-Rand" $ssp_dir/$func/ssp-rand --baseline "GP-Sinc" $sinc_gp_dir/$func/gp-sinc | tee -a $func-stats.txt
 
-	python compare-algs.py --alg "SSP-Hex" $ssp_dir/$func/ssp-hex  --baseline "GP-Sinc" $sinc_gp_dir/$func/static-gp | tee -a $func-stats.txt
+	python compare-algs.py --alg "SSP-Hex" $ssp_dir/$func/ssp-hex  --baseline "GP-Sinc" $sinc_gp_dir/$func/gp-sinc | tee -a $func-stats.txt
 
-	python compare-algs.py --alg "GP-Sinc" $sinc_gp_dir/$func/static-gp --baseline "GP-Matern" $matern_gp_dir/$func/matern.static-gp | tee -a $func-stats.txt
+	python compare-algs.py --alg "GP-Sinc" $sinc_gp_dir/$func/gp-sinc --baseline "GP-Matern" $matern_gp_dir/$func/gp-matern | tee -a $func-stats.txt
 
 done
 
