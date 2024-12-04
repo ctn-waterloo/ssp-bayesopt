a82bc8c113e2080a1224fbbf6eb650741406c66f
P. Michael Furlong <michael.furlong@uwaterloo.ca>
2024-12-04 10:33:59 -0500

    modified the var-dimensionality-sensitivity code

--- experiments/var-dimensionality-sensitivity.py
+++ experiments/var-dimensionality-sensitivity.py
@@ -7,16 +7,21 @@ import os
 import numpy as np
 
 func_name="styblinski-tang" # rastrigin , styblinski-tang  rosenbrock
+# func_name="rastrigin" # rastrigin , styblinski-tang  rosenbrock
 dims = np.arange(1,10,1)#,6,7,8,10,12,14,16,18])
 n_scales = 4
 ns =np.ceil((250 - 1)/(2*n_scales*(dims+1)))
 ssp_dims = (2*(dims+1)*(ns*n_scales) + 1).astype(int)
 
-ssp_dims = 1 + 2**np.array([5, 6, 7, 8, 9, 10])
+ssp_dims = 2**np.array([5, 6, 7, 8, 9, 10])
+# ssp_dims = np.array([1024])
 # ssp_dim = 300
 
 #         dest_dir="/home/ns2dumon/Documents/ssp-bayesopt/data/var-dim-sensitivity/" + str(dim) + "/"
-for i,dim in enumerate(dims):
+
+num_trials = 10
+# for i,dim in enumerate(dims):
+for i,dim in enumerate([8,9]):
     # run the gp model
     dest_dir=f"/run/media/furlong/Data/bo-ssp/var-dim-sensitivity/{dim}/" 
     os.system("python run_agent.py --agent gp-matern" 
@@ -24,14 +29,20 @@ for i,dim in enumerate(dims):
             + " --data-dir " + dest_dir 
             + " --len-scale -1 "
         )
+    os.system("python run_agent.py --agent gp-sinc" 
+            + f" --num-trials {num_trials} --func " + func_name + str(dim) 
+            + " --data-dir " + dest_dir 
+            + f" --len-scale {len_scale} "
+            + f" --len-scale -1 "
+        )
     # run the ssp models
     for ssp_dim in ssp_dims:
         ssp_dest_dir = dest_dir + f'{ssp_dim}/'
         for agt in ["ssp-rand"]: #"ssp-hex", "gp-sinc" 
             os.system("python run_agent.py --agent " + agt
-                    + " --num-trials 30 --func " + func_name + str(dim) 
+                    + f" --num-trials {num_trials} --func " + func_name + str(dim) 
                     + " --data-dir " + ssp_dest_dir 
-                    + " --len-scale -1 --ssp-dim " + str(ssp_dim)
+                    + f" --len-scale -1 --ssp-dim " + str(ssp_dim)
             )
         ### end for agt in ['ssp-rand']
     ### end for ssp_dim in ssp_dims
