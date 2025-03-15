66fc573979d5ced43af5292e716df81c20008ed2
Nicole Dumont <nicole.s.dumont@gmail.com>
2025-03-15 18:03:59 -0400

    Update mcbo_runs.sh

--- experiments/mcbo_runs.sh
+++ experiments/mcbo_runs.sh
@@ -2,13 +2,13 @@
 #SBATCH --nodes=1
 #SBATCH --mem=2G
 #SBATCH --time=0-6:00
-#SBATCH --array=0-2
+#SBATCH --array=0-5
 
 module load python/3.10 scipy-stack
 source ~/projects/def-celiasmi/ns2dumon/mcbo-env/bin/activate
 
-run_index=$(( SLURM_ARRAY_TASK_ID / 5 ))
-func_index=$(( SLURM_ARRAY_TASK_ID % 5 ))
+run_index=$(( SLURM_ARRAY_TASK_ID / 3 ))
+func_index=$(( SLURM_ARRAY_TASK_ID % 3 ))
 
 sspdim=97
 dest_dir="data"
@@ -20,5 +20,5 @@ if [ "$run_index" -eq 0 ]; then
     python run_agent_mcbo.py --task-id $func --num-trials 30 --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb 30.
 elif [ "$run_index" -eq 1 ]; then
     echo "Running loihi-sim"
-    python run_agent_mcbo.py --task-id $func --num-trials 30 --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb 30. --nengo --backend "loihi-sim"
+    python run_agent_mcbo.py --task-id $func --num-trials 10 --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb 30. --nengo --backend "loihi-sim"
 fi
