431914fc543fc9f5210862f7892824350dfb6958
Nicole Dumont <ns2dumon@cedar1.int.cedar.computecanada.ca>
2025-04-22 10:49:07 -0700

    SLURM scripts

--- experiments/ucb_runs.sh
+++ experiments/ucb_runs.sh
@@ -2,34 +2,38 @@
 #SBATCH --nodes=1
 #SBATCH --mem=2G
 #SBATCH --time=0-4:00
-#SBATCH --array=0-4
+#SBATCH --array=0-15
 
 module load python/3.10 scipy-stack
 source ~/projects/def-celiasmi/ns2dumon/bosspenv/bin/activate
 
-func_index=$(( SLURM_ARRAY_TASK_ID / 5 ))
-run_index=$(( SLURM_ARRAY_TASK_ID % 5 ))
+func_index=$(( SLURM_ARRAY_TASK_ID % 3 ))
+run_index=$(( SLURM_ARRAY_TASK_ID / 3 ))
 
+beta=1
 sspdim=97
-dest_dir="./data"
+dest_dir="./data2"
 funcs=("branin-hoo" "himmelblau" "goldstein-price")
 func="${funcs[$func_index]}"
 
 if [ "$run_index" -eq 0 ]; then
     echo "Running ssp-rand"
-    python run_agent.py --agent "ssp-rand" --num-trials 30 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb 0.1
+    python run_agent.py --agent "ssp-rand" --num-trials 30 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb $beta
 elif [ "$run_index" -eq 1 ]; then
     echo "Running ssp-hex"
-    python run_agent.py --agent "ssp-hex" --num-trials 30 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb 0.1
+    python run_agent.py --agent "ssp-hex" --num-trials 30 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb $beta
 elif [ "$run_index" -eq 2 ]; then
     echo "Running gp-sinc"
-    python run_agent.py --agent "gp-sinc" --num-trials 30 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb 0.1
+    python run_agent.py --agent "gp-sinc" --num-trials 30 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb $beta
 elif [ "$run_index" -eq 3 ]; then
     echo "Running gp-matern"
-    python run_agent.py --agent "gp-matern" --num-trials 30 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb 0.1
+    python run_agent.py --agent "gp-matern" --num-trials 30 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb $beta
 elif [ "$run_index" -eq 4 ]; then
+    echo "Running rff"
+    python run_agent.py --agent "rff" --ssp-dim $sspdim --num-trials 30 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb $beta
+elif [ "$run_index" -eq 5 ]; then
     echo "Running ssp-hex-loihi"
-    python run_agent.py --agent "ssp-hex" --num-trials 30 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb 0.1 --nengo --backend "loihi-sim"
+    #python run_agent.py --agent "ssp-hex" --num-trials 10 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $sspdim --gamma 0.0 --beta-ucb $beta --nengo --backend "loihi-sim" --num-neurons 8
 else
     echo "Unexpected run_index value: $run_index"
 fi
--- experiments/var-dimsensitivity.sh
+++ experiments/var-dimsensitivity.sh
@@ -1,38 +1,45 @@
+
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --mem=2G
 #SBATCH --time=0-6:00
-#SBATCH --array=0-13
+#SBATCH --array=14-29
 
 module load python/3.10 scipy-stack
 source ~/projects/def-celiasmi/ns2dumon/bosspenv/bin/activate
 
 
-agt_index=$(( SLURM_ARRAY_TASK_ID / 7 ))
-dim_index=$(( SLURM_ARRAY_TASK_ID % 7 ))
+agt_index=$(( SLURM_ARRAY_TASK_ID / 6 ))
+dim_index=$(( SLURM_ARRAY_TASK_ID % 6 ))
 
-beta=1.0
-vardims=(1 2 3 4 5 6 7)
+beta=10.
+vardims=(1 2 3 4 5 6)
 dim="${vardims[dim_index]}"
 
 func="styblinski-tang$dim"
 nscales=3
 
-for sspdim in 100 250 500
-	do
-	  nrotates=$(python -c "import numpy as np; print(int(np.round(($sspdim - 1) / (2 * $nscales * ($dim + 1)))))")
-    dest_dir="data/sspdim$sspdim_vardim$dim$"
+for sspdim in 150 250 550
+do
+    nrotates=$(python -c "import numpy as np; print(int(np.round(($sspdim - 1) / (2 * $nscales * ($dim + 1)))))")
+    realsspdim=$(python -c "import numpy as np; print(int(np.round(1 + (2 * $nscales * $nrotates * ($dim + 1)))))")
+    dest_dir="data/sspdim{$realsspdim}"
     if [ "$agt_index" -eq 0 ]; then
         echo "Running ssp-rand with target sspdim of $sspdim"
-        python run_agent.py --agent "ssp-rand" --num-trials 10 --func $func --data-dir $dest_dir --len-scale -1 --n-scales $nscales --n-rotates $nrotates --gamma 0.0 --beta-ucb $beta
+        python run_agent.py --agent "ssp-rand" --num-trials 10 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $realsspdim --gamma 0.0 --beta-ucb $beta
     elif [ "$agt_index" -eq 1 ]; then
         echo "Running ssp-hex with target sspdim of $sspdim"
         python run_agent.py --agent "ssp-hex" --num-trials 10 --func $func --data-dir $dest_dir --len-scale -1 --n-scales $nscales --n-rotates $nrotates --gamma 0.0 --beta-ucb $beta
-    elif [ "$agt_index" -eq 2 ]; then
-      echo "Running gp-sinc"
-      python run_agent.py --agent "gp-sinc" --num-trials 10 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb $beta
-    elif [ "$agt_index" -eq 3 ]; then
-        echo "Running gp-matern"
-        python run_agent.py --agent "gp-matern" --num-trials 10 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb $beta
+    elif [ "$agt_index" -eq 4 ]; then
+        echo "Running rff with target sspdim of $sspdim"
+        python run_agent.py --agent "rff" --num-trials 10 --func $func --data-dir $dest_dir --len-scale -1 --ssp-dim $realsspdim --gamma 0.0 --beta-ucb $beta
     fi
 done
+dest_dir="data"
+if [ "$agt_index" -eq 2 ]; then
+      echo "Running gp-sinc"
+      python run_agent.py --agent "gp-sinc" --num-trials 10 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb $beta
+elif [ "$agt_index" -eq 3 ]; then
+        echo "Running gp-matern"
+        python run_agent.py --agent "gp-matern" --num-trials 10 --func $func --data-dir $dest_dir --gamma 0.0 --beta-ucb $beta
+fi
