5d3f07f663f88f334ea4f1bae8b9d13af965f928
Nicole Dumont <nicole.s.dumont@gmail.com>
2025-04-23 15:15:08 -0400

    Code for rough energy estimations for different backends

--- ssp_bayes_opt/__init__.py
+++ ssp_bayes_opt/__init__.py
@@ -1,3 +1,4 @@
 from .bayesian_optimization import BayesianOptimization
 from .nengo_bayesian_optimization import NengoBayesianOptimization
+from .network_solver import make_network
 from .agents import *
